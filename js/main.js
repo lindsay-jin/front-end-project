318b73ac00deba13cad9d8d55d7ddb3aa76d814a
lindsay-jin <lindsayjin1@gmail.com>
2024-03-07 17:45:36 +0000

    view swapping from landing to search page by clicking search button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+"use strict";
 //landing page
 const $startSearchButton = document.querySelector('.start-search-button');
 const $loginButton = document.querySelector('.login-button');
-
 //search page
 const $location = document.querySelector('.location-input');
 const $keyword = document.querySelector('.keyword-input');
@@ -9,12 +9,10 @@ const $open = document.querySelector('#open');
 const $show = document.querySelector('.show-input');
 const $sort = document.querySelector('#sort');
 const $resetButton = document.querySelector('.reset-button');
-const $submitButton = document.querySelector('.submit-button')
-
+const $submitButton = document.querySelector('.submit-button');
 const $viewLanding = document.querySelector('.view-landing');
 const $viewSearch = document.querySelector('.view-search');
-
-$startSearchButton?.addEventListener('click', ()=>{
-  $viewLanding?.classList.add('hidden');
-  $viewSearch?.classList.remove('hidden');
-})
+$startSearchButton?.addEventListener('click', () => {
+    $viewLanding?.classList.add('hidden');
+    $viewSearch?.classList.remove('hidden');
+});
