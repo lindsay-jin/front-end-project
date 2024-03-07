82a24bd29235783fe63e1203eddc272e5ce78a1d
lindsay-jin <lindsayjin1@gmail.com>
2024-03-07 00:58:16 +0000

    working on layout

---
+++ ts/main.ts
@@ -0,0 +1,7 @@
+const $location = document.querySelector('.location-input');
+const $keyword = document.querySelector('.keyword-input');
+const $open = document.querySelector('#open');
+const $show = document.querySelector('.show-input');
+const $sort = document.querySelector('#sort');
+const $resetButton = document.querySelector('.reset-button');
+const $submitButton = document.querySelector('.submit-button')
