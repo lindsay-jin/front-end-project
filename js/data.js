7974bf3df13cd6e11c48c2c27ac792c641c658ed
lindsay-jin <lindsayjin1@gmail.com>
2024-03-13 23:08:11 +0000

    desktop layout for details page

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 /* exported data */
 let data = {
-  likedEntries: [],
+    likedEntries: [],
 };
 window.addEventListener('beforeunload', () => {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem('data-model', dataJSON);
-  //stores the serialized data in the browser's local storage under the key 'data-model'
+    const dataJSON = JSON.stringify(data);
+    localStorage.setItem('data-model', dataJSON);
+    // stores the serialized data in the browser's local storage under the key 'data-model'
 });
 const storedData = localStorage.getItem('data-model');
 if (storedData) {
-  data = JSON.parse(storedData);
+    data = JSON.parse(storedData);
 }
